872ad2503a710010e467275682e60e29c6ae1325
XuRui <XuRui@DESKTOP-O9T02HG>
2019-12-02 14:50:29 +0800

    update malloc

--- include/BlockBitmap.h
+++ include/BlockBitmap.h
@@ -20,12 +20,14 @@ class BlockBitmap {
 private:
     char* bits_;
     int N;
+    int offset_;
     //boost::detail::spinlock bits_lock_;
 protected:
     void init(uint64_t n){
         N = n/8 + 1;
         bits_ = (char*)malloc(N);    // allocated memory
         memset(bits_,0,N);  // init the bitsets
+        offset_ = 0;
     }
 public:
     BlockBitmap(uint64_t num){
